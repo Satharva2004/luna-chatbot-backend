0120c3282a6368207e337a961f1321fd85b44a18
Satharva2004 <satharva2004@gmail.com>
2026-01-04 22:11:43 +0530

    feat: add GitHub Actions CI/CD workflow and PM2 configuration for server deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [{
+        name: "luna-backend",
+        script: "./src/server.js",
+        instances: "max",
+        exec_mode: "cluster",
+        env: {
+            NODE_ENV: "development",
+        },
+        env_production: {
+            NODE_ENV: "production",
+        }
+    }]
+}
